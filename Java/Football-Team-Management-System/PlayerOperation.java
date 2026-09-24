90f92d21c5f0a636b8758442116cffbcea834d31
mr-worst69 <yasin824472@gmail.com>
2026-09-24 18:26:11 +0600

    Update PlayerOperation.java with source code

--- Java/Football-Team-Management-System/PlayerOperation.java
+++ Java/Football-Team-Management-System/PlayerOperation.java
@@ -1,1 +1,22 @@
+public interface PlayerOperation {
 
+    // Add a new player
+    void addPlayer();
+
+
+    // Search player by ID
+    void searchPlayer(int id);
+
+
+    // Search player by name
+    void searchPlayer(String name);
+
+
+    // Delete player information
+    void deletePlayer();
+
+
+    // Display all players
+    void displayPlayers();
+
+}
