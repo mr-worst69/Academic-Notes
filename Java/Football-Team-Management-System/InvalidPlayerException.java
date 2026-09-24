7aae3c445d6380f98df0739f06398563ea9863fa
mr-worst69 <yasin824472@gmail.com>
2026-09-24 18:28:14 +0600

    Update InvalidPlayerException.java with source code

--- Java/Football-Team-Management-System/InvalidPlayerException.java
+++ Java/Football-Team-Management-System/InvalidPlayerException.java
@@ -1,1 +1,10 @@
+public class InvalidPlayerException extends Exception {
 
+    // Constructor to show custom error message
+    public InvalidPlayerException(String message) {
+
+        super(message);
+
+    }
+
+}
