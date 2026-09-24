7c4f78db359f6a0e3a5b13e70cc5783c5ad84831
mr-worst69 <yasin824472@gmail.com>
2026-09-24 18:25:39 +0600

    Update Player.java with source code

--- Java/Football-Team-Management-System/Player.java
+++ Java/Football-Team-Management-System/Player.java
@@ -1,1 +1,68 @@
+public class Player extends Person {
 
+    // Encapsulation: Player specific data
+    private int playerId;
+    private String position;
+    private int jerseyNumber;
+    private int goals;
+
+
+    // Constructor
+    public Player(int playerId, String name, int age, String phone,
+                  String position, int jerseyNumber, int goals) {
+
+        super(name, age, phone);
+        // Calling parent class constructor
+
+        this.playerId = playerId;
+        this.position = position;
+        this.jerseyNumber = jerseyNumber;
+        this.goals = goals;
+
+    }
+
+
+    // Getter method
+    public int getPlayerId() {
+
+        return playerId;
+
+    }
+
+
+    public String getPosition() {
+
+        return position;
+
+    }
+
+
+    public int getJerseyNumber() {
+
+        return jerseyNumber;
+
+    }
+
+
+    public int getGoals() {
+
+        return goals;
+
+    }
+
+
+    // Method Overriding
+    @Override
+    public void displayInfo() {
+
+        System.out.println("\nPlayer ID: " + playerId);
+        System.out.println("Name: " + getName());
+        System.out.println("Age: " + getAge());
+        System.out.println("Phone: " + getPhone());
+        System.out.println("Position: " + position);
+        System.out.println("Jersey Number: " + jerseyNumber);
+        System.out.println("Goals: " + goals);
+
+    }
+
+}
