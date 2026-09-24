75935ac7ae8de9f7f5d0d201993625fe68ef6994
mr-worst69 <yasin824472@gmail.com>
2026-09-24 18:24:49 +0600

    Update Person.java with source code

--- Java/Football-Team-Management-System/Person.java
+++ Java/Football-Team-Management-System/Person.java
@@ -1,1 +1,44 @@
+public abstract class Person {
 
+    // Encapsulation: private variables
+    private String name;
+    private int age;
+    private String phone;
+
+
+    // Constructor
+    public Person(String name, int age, String phone) {
+
+        this.name = name;
+        this.age = age;
+        this.phone = phone;
+
+    }
+
+
+    // Getter methods
+    public String getName() {
+
+        return name;
+
+    }
+
+
+    public int getAge() {
+
+        return age;
+
+    }
+
+
+    public String getPhone() {
+
+        return phone;
+
+    }
+
+
+    // Abstract method
+    public abstract void displayInfo();
+
+}
